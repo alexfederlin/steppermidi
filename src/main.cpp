83e9160a290a8ab0e8ed6a4243a4322d8473309b
Alexander Federlin <alexander.federlin@ericsson.com>
2024-02-13 22:15:37 +0100

    working iteration

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,15 @@
 #include <WiFi.h>
 #include <TimeLib.h>
 #include <TelnetStream.h>
+#include <math.h> 
 
 #define LED 2
+#define DIR_PIN 2
+#define STEP_PIN 0
 
+bool noteon = false;
+bool dir = false;
+int interval = 0;
 
 const long gmtOffset_sec = 3600;
 const int daylightOffset_sec = 3600;
@@ -16,7 +22,15 @@ const int daylightOffset_sec = 3600;
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 #include <secrets.h>
 
+u_int32_t intervals[128];
 
+
+// from https://gist.github.com/YuxiUx/ef84328d95b10d0fcbf537de77b936cd
+// calculate Interval in microseconds
+float pitchToInterval(int note) {
+    float a = 440; //frequency of A (coomon value is 440Hz)
+    return floor(1000000/((a / 32) * pow(2, ((note - 9) / 12.0))));
+}
 
 // Create and bind the MIDI interface to the default hardware Serial port
 struct CustomBaudRateSettings : public MIDI_NAMESPACE::DefaultSerialSettings {
@@ -43,26 +57,35 @@ static void log(byte pitch) {
   TelnetStream.print(" ");
   TelnetStream.print(timeStr);
   TelnetStream.print(" Pitch: ");
-  TelnetStream.println(pitch);
+  TelnetStream.print(pitch);
+  TelnetStream.print(" Freq: ");
+  TelnetStream.println(intervals[pitch]);
 }
 
 
 void doSomeStuffWithNoteOn(byte channel, byte pitch, byte velocity)
 {
-  //digitalWrite(LED, !digitalRead(LED));
   log (pitch);  
-  digitalWrite(LED, HIGH);
-  // TelnetStream.println(pitch);
+  //  digitalWrite(LED, HIGH);
+  noteon=true;
+  interval=intervals[pitch];
 }
 
 void doSomeStuffWithNoteOff(byte channel, byte pitch, byte velocity)
 {
-  digitalWrite(LED, LOW);
+  //digitalWrite(LED, LOW);
+  noteon=false;
+  dir = !dir;
+  digitalWrite(DIR_PIN, dir);
 }
 
 
 void setup() {
   // Serial.begin(115200);
+  pinMode(STEP_PIN, OUTPUT);
+  digitalWrite(STEP_PIN,LOW);
+
+  pinMode(DIR_PIN, OUTPUT);
 
   pinMode(LED,OUTPUT);
   digitalWrite(LED,LOW);
@@ -97,6 +120,12 @@ void setup() {
 
   digitalWrite(LED,HIGH);
 
+  // create lookup table for frequencies
+  for (int pitch = 1; pitch < 128; pitch++){
+    intervals[pitch] = pitchToInterval(pitch);
+    log(pitch);
+  }
+
   MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all incoming messages
   MIDI.setHandleNoteOn(doSomeStuffWithNoteOn);
   MIDI.setHandleNoteOff(doSomeStuffWithNoteOff);
@@ -105,10 +134,21 @@ void setup() {
 void loop() {
 
   MIDI.read();
+
+  if (noteon){
+    digitalWrite(STEP_PIN,HIGH);
+    delayMicroseconds(interval);
+    digitalWrite(STEP_PIN,LOW);
+    delayMicroseconds(interval);
+  }
+
+
   byte i = 0;
   switch (TelnetStream.read()) {
     case 'L':
-      log(i);
+      for (i=40; i<80; i++){
+        log(i);
+      }
       digitalWrite(LED,LOW);
       delay(100);
       digitalWrite(LED,HIGH);
